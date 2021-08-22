80329d9fabdf5cd4db2563dbd9c3d430fab690c4
ray1985113 <ray1985113@qq.com>
2021-08-22 22:39:01 +0800

    1.响应式current 2.根据hash响应式变换component

--- custom-vue-route/src/App.vue
+++ custom-vue-route/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
@@ -18,6 +21,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- custom-vue-route/src/router/index.js
+++ custom-vue-route/src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import VueRouter from './custom-vue-router'
+import Home from '../views/Home.vue'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
-  routes: [
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/About.vue')
     }
-  ]
+]
+
+
+const router = new VueRouter({
+    routes
 })
+
+export default router
